8e1b6a36a4dc5c0f982cea1cd7d5e487753938a9
Arvind Satyanarayan <arvind.2111@gmail.com>
2005-05-06 14:56:51 +0000

    Fixed Plugin Registeration

--- plugins/RebuildBlogs/rebuildblogs.pl
+++ plugins/RebuildBlogs/rebuildblogs.pl
@@ -4,12 +4,12 @@ use strict;
 use MT;
 use MT::Plugin;
 use vars qw($VERSION);
-$VERSION = '0.1';
+$VERSION = '1.0';
 my $about = {
 	dir => 'RebuildBlogs',
-  name => 'Rebuild Multiple Blogs',
+  name => 'MT RebuildBlogs v'.$VERSION,
   config_link => 'mt-rebuild-blogs.cgi',
-  description => 'Adding the ability to rebuild multiple blogs at once.',
-  doc_link => 'http://www.papascott.de/archives/2004/08/12/hello-world-as-an-mtapp-cgi/'
+  description => 'Adds the ability to rebuild multiple blogs at once.',
+  doc_link => 'http://www.movalog.com:82/cgi-bin/trac.cgi/wiki/MtRebuildBlogs'
 }; 
 MT->add_plugin(new MT::Plugin($about));
